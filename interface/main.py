999770b8a96d05704029d98eda773b962033cd30
Aleksei Glebov <aglebov@bu.edu>
2024-10-29 15:52:10 -0400

    Updated interface/main.py; Learning syntax

--- interface/main.py
+++ interface/main.py
@@ -2,33 +2,38 @@
 #       pip install pyside6
 #
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class MyWidget(QtWidgets.QWidget):
+class MainTab(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        # Universal stylesheet.
+        universal_stylesheet =  """font-size: 32px; font-family: Times New Roman; background-color: green;"""
+        # Exit button.
+        self.exit_button = QtWidgets.QPushButton("Exit")
+        self.exit_button.setStyleSheet(universal_stylesheet)
 
-        self.button = QtWidgets.QPushButton("Click me!")
         self.text = QtWidgets.QLabel("Hello World",
                                      alignment=QtCore.Qt.AlignCenter)
+        self.text.setStyleSheet(universal_stylesheet)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
+        self.layout.addWidget(self.exit_button)  # Terminate the program.
+        
+        self.exit_button.clicked.connect(self.exit_button_clicked) # Call termination function.
 
     @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+    
+    # Termination function to finish program.
+    def exit_button_clicked(self):
+        sys.exit() # Is that right termination?
         
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = MyWidget()
+    widget = MainTab()
     widget.resize(800, 600)
     widget.show()
 
